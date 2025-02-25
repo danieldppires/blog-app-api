2201ef619621e629e44a5ed6e161acff4155eddd
Daniel Pires <danieldppires@gmail.com>
2025-02-25 13:50:47 -0300

    Salvando e buscando comentários

---
+++ src/controllers/comment.controller.ts
@@ -0,0 +1,84 @@
+import { NextFunction, Request, Response } from "express";
+import Comment from "../models/comment.model";
+import User from "../models/user.model";
+
+interface AuthenticatedRequest extends Request {
+	auth?: { userId?: string };
+}
+
+export const getPostComments = async (req: Request, res: Response, next: NextFunction) => {
+	try {
+		const comments = await Comment.find({ post: req.params.postId })
+			.populate("user", "username img")
+			.sort({ createdAt: -1 });
+
+		res.json(comments);
+	}
+	catch (err) {
+		next(err);
+	}
+}
+
+export const addComment = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const clerkUserId = req.auth?.userId;
+		const postId = req.params.postId;
+
+		if (!clerkUserId) {
+			res.status(401).json("Not authenticated");
+			return;
+		}
+
+		const user = await User.findOne({ clerkUserId });
+		if (!user) {
+			res.status(404).json("User not found");
+			return;
+		}
+
+		const newComment = new Comment({
+			...req.body,
+			user: user._id,
+			post: postId,
+		});
+
+		const savedComment = await newComment.save();
+
+		res.status(201).json(savedComment);
+	}
+	catch (err) {
+		next(err);
+	}
+}
+
+export const deleteComment = async (req: AuthenticatedRequest, res: Response, next: NextFunction): Promise<void> => {
+	try {
+		const clerkUserId = req.auth?.userId;
+		const commentId = req.params.id;
+
+		if (!clerkUserId) {
+			res.status(401).json("Not authenticated") 
+			return;
+		};
+
+		const user = await User.findOne({ clerkUserId });
+		if (!user) {
+			res.status(404).json("User not found") 
+			return;
+		}
+
+		const deletedComment = await Comment.findOneAndDelete({
+			_id: commentId,
+			user: user._id,
+		});
+
+		if (!deletedComment) {
+			res.status(403).json("You can delete only your comment!");
+			return;
+		}
+
+		res.status(200).json("Comment deleted");
+	}
+	catch (err) {
+		next(err);
+	}
+}
